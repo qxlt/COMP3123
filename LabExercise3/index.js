aa4667f0af32f408d46f1b12937ba792511fee68
qxlt <liuqx3@gmail.com>
2024-09-19 20:37:29 -0400

    small error fixed

--- LabExercise3/index.js
+++ LabExercise3/index.js
@@ -20,14 +20,14 @@ const server = http.createServer((req, res) => {
             res.write('<h1>Welcome to Lab Exercise 03</h1>');
         }
 
-        if (req.url === '/employee') {
+        else if (req.url === '/employee') {
             //TODO - Display all details for employees in JSON format
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write('<h1>Welcome to Emplyee Page</h1>');
             res.write(JSON.stringify(employees));
         }
 
-        if (req.url === '/employee/names') {
+        else if (req.url === '/employee/names') {
             //TODO - Display only all employees {first name + lastname} in Ascending order in JSON Array
             //e.g. [ "Ash Lee", "Mac Mohan", "Pritesh Patel"]
             res.writeHead(200, {'Content-Type': 'text/html'});
@@ -40,7 +40,7 @@ const server = http.createServer((req, res) => {
             res.write(JSON.stringify(fullName));
         }
 
-        if (req.url === '/employee/totalsalary') {
+        else if (req.url === '/employee/totalsalary') {
             //TODO - Display Sum of all employees salary in given JSON format 
             //e.g. { "total_salary" : 100 }  
             res.writeHead(200, {'Content-Type': 'text/html'});
@@ -53,8 +53,12 @@ const server = http.createServer((req, res) => {
                 'total_salary' : totalSalary
             }
             res.write(JSON.stringify(sum));
-    }
-    res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
+        }
+        else{
+            res.statusCode = 404;
+            res.end('<h1>404 - Page Not Found</h1>');
+        }
+        res.end();
     }
 })
 
