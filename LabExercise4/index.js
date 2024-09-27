76a38f1cf78919f67894922b7d2b819071c21225
qxlt <liuqx3@gmail.com>
2024-09-26 22:31:41 -0400

    Lab4

---
+++ LabExercise4/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const SERVER_PORT = 3001;
+
+app.get('/hello', (request, response) =>{
+    response.send('<h1>Hello Express Js</h1>');
+})
+
+app.get('/user', (request, response) => {
+    console.log(request.query)
+    const fnm = request.query.fnm;
+    const lnm = request.query.lnm;
+    response.send(`First Name: ${fnm}, Last Name: ${lnm}`);
+});
+
+// app.get('/user', (request, response) => {
+//     console.log(request.query);
+//     const fnm = request.query.fnm;
+//     const lnm = request.query.lnm;
+//     response.send(`First Name: ${fnm}, Last Name: ${lnm}`);
+// });
+
+app.post('/user/:fnm/:lnm', (req, res)=>{
+    console.log(req.params);
+    const fnm = req.params.fnm;
+    const lnm = req.params.lnm;
+    const user = {
+        firstname: fnm,
+        lastname: lnm
+    }
+    res.send(user);
+})
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+});
